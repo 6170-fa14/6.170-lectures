b6c495bef7fbc3cda28cf8749c97251d08aaf9c0
Adam Chlipala <adam@chlipala.net>
2014-08-22 14:04:56 -0400

    One-page Node.js example

---
+++ L01-Intro/onepage.js
@@ -0,0 +1,39 @@
+// Import the Web-serving module.
+var http = require("http");
+
+// Function called on every HTTP request
+function handle(request, response) {
+    // First, tell the client that we will be sending HTML.
+    response.writeHead(200, {"Content-Type": "text/html"});
+
+    // Next, write the HTML document.
+    response.write('<!DOCTYPE html>'
+                   + '<html>'
+                   + '<head>'
+                   + '<title>Hello, 6.170 World!</title>'
+                   + '<meta charset="UTF-8">'
+                   + '<script type="text/javascript">'
+                   + 'var counter = 0;'
+                   + 'function changeit() {'
+                   + 'var text = document.getElementById("text");'
+                   + 'var changeto = document.getElementById("changeto");'
+                   + 'text.innerHTML = "#" + counter + ": " + changeto.value;'
+                   + 'counter++;'
+                   + '}'
+                   + '</script>'
+                   + '</head>'
+                   + '<body>'
+                   + '<h1>Hello, <a href="https://stellar.mit.edu/S/course/6/fa14/6.170/">6.170</a> World!</h1>'
+                   + '<button onclick="changeit()">Set text to:</button> <input type="text" id="changeto">'
+                   + '<p id="text">I\'m <i>waiting</i>!</p>'
+                   + '</body>'
+                   + '</html>');
+
+    // Indicate that the response is fully determined now.
+    response.end();
+}
+
+// Create a new Web server that calls 'handle' on every request.
+var server = http.createServer(handle);
+// Start that server listening on TCP port 8080.
+server.listen(8080);
