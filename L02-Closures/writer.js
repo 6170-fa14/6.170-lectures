ab55569d5d8713e5b5a05816f5ca59de26759c8e
Adam Chlipala <adam@chlipala.net>
2014-09-06 15:10:51 -0400

    L02 simple writer example

---
+++ L02-Closures/writer.js
@@ -0,0 +1,4 @@
+// A trivial search function, which probably does not promote user satisfaction
+function search(text, output) {
+    output("Oh, you want to search for '" + text + "'?  How interesting!");
+}
